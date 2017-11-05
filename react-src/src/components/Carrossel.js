50e42c6b79f208d43ae1dd017dc135b214b1b742
vmichelan <venanciuslm@gmail.com>
2017-11-05 17:40:34 -0200

    Fix das imagens do carrossel não aparecerem

--- react-src/src/components/Carrossel.js
+++ react-src/src/components/Carrossel.js
@@ -3,6 +3,13 @@ import Carousel from 'nuka-carousel';
 import coffe from '../img/cafeteria.jpg';
 
 class Carrossel extends React.Component {
+  //redimensiona o carrossel depois da imagem ser carregada para evitar o problema do carrossel ficar com tamanho 0
+  _handleImageLoaded(){
+    setTimeout(() => {
+    window.dispatchEvent(new Event('resize'));
+    }, 0);
+  }
+
   render(){
     var settings = {
       autoplay: true,
@@ -10,9 +17,10 @@ class Carrossel extends React.Component {
       cellSpacing: 20,
       wrapAround: true,
     };
+
     return (
       <Carousel {...settings}>
-        <img src={require('../img/cafeteria.jpg')}/>
+        <img src={require('../img/cafeteria.jpg')} onLoad={this._handleImageLoaded}/>
         <img src={coffe}/>
         <img src={coffe}/>
         <img src={coffe}/>
