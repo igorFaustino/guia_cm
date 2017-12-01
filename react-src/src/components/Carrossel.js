15eb1d84742059ef31c6905579b5d4902f04ef21
Igor Neves Faustino <igornfaustino@gmail.com>
2017-12-01 01:57:07 -0200

    carroussel funcionando

--- react-src/src/components/Carrossel.js
+++ react-src/src/components/Carrossel.js
@@ -1,14 +1,21 @@
 import React, {Component} from 'react';
+import {Button} from 'reactstrap';
 import Carousel from 'nuka-carousel';
 import coffe from '../img/cafeteria.jpg';
 
 class Carrossel extends React.Component {
+  constructor(){
+    super();
+    this.index = 0
+  }
+
   //redimensiona o carrossel depois da imagem ser carregada para evitar o problema do carrossel ficar com tamanho 0
   _handleImageLoaded(){
     setTimeout(() => {
     window.dispatchEvent(new Event('resize'));
     }, 0);
   }
+
 
   render(){
     var settings = {
@@ -17,10 +24,11 @@ class Carrossel extends React.Component {
       cellSpacing: 20,
       wrapAround: true,
     };
-
+    
     return (
-      <Carousel {...settings}>
-        <img src={require('../img/cafeteria.jpg')} onLoad={this._handleImageLoaded}/>
+      <Carousel {...settings} afterSlide={(newSlideIndex) => {this.props.teste(newSlideIndex)}}>
+        
+      <img src={require('../img/cafeteria.jpg')} onLoad={this._handleImageLoaded}/>
         <img src={coffe}/>
         <img src={coffe}/>
         <img src={coffe}/>
--- react-src/src/pages/Inicio.js
+++ react-src/src/pages/Inicio.js
@@ -23,81 +23,123 @@ import { Row, Col, Container } from 'reactstrap';
 // } from 'react-router-dom'
 
 class Inicio extends Component {
-	constructor(){
+	constructor() {
 		super();
 		this.state = {
+			categories: [],
+			indice: 0,
+			tamanho: 6,
+			v: [
+				{
+					nome: 'LUGAR 1',
+					desc: 'DESC 1'
+				},
+				{
+					nome: 'LUGAR 2',
+					desc: 'DESC 2'
+				},
+				{
+					nome: 'LUGAR 3',
+					desc: 'DESC 3'
+				},
+				{
+					nome: 'LUGAR 4',
+					desc: 'DESC 4'
+				},
+				{
+					nome: 'LUGAR 5',
+					desc: 'DESC 5'
+				},
+				{
+					nome: 'LUGAR 6',
+					desc: 'DESC 6'
+				},
+			]
 		}
+
+		this.nextSlide = this.nextSlide.bind(this);
 	}
 
-	componentWillMount(){
+	componentWillMount() {
 		this.setState(
-			{categories: [
-				{
-					nome: 'Atividades fisicas',
-					img: require('../img/atividade.jpg')
-				},
-				{
-					nome: 'Comer no Domingo',
-					img: require('../img/domingo.jpg')
-				},
-				{
-					nome: 'Imobiliária',
-					img: require('../img/imobiliaria.jpg')
-				},
-				{
-					nome: 'Livraria',
-					img: require('../img/sebo.jpg')
-				},
-				{
-					nome: 'Hospitais e Farmácias',
-					img: require('../img/farmacia.jpg')
-				},
-				{
-					nome: 'Banco',
-					img: require('../img/dinheiro.jpg')
-				},
-				{
-					nome: 'Moveis Usados',
-					img: require('../img/moveis.jpg')
-				},
-				{
-					nome: 'Mercados baratos',
-					img: require('../img/mercado.jpg')
-				}
-			]
+			{
+				indice: 0,
+				categories: [
+					{
+						nome: 'Atividades fisicas',
+						img: require('../img/atividade.jpg')
+					},
+					{
+						nome: 'Comer no Domingo',
+						img: require('../img/domingo.jpg')
+					},
+					{
+						nome: 'Imobiliária',
+						img: require('../img/imobiliaria.jpg')
+					},
+					{
+						nome: 'Sebo',
+						img: require('../img/sebo.jpg')
+					},
+					{
+						nome: 'Hospitais e Farmácias',
+						img: require('../img/farmacia.jpg')
+					},
+					{
+						nome: 'Banco',
+						img: require('../img/dinheiro.jpg')
+					},
+					{
+						nome: 'Moveis Usados',
+						img: require('../img/moveis.jpg')
+					},
+					{
+						nome: 'Mercados baratos',
+						img: require('../img/mercado.jpg')
+					}
+				]
+			});
+		console.log(this.state);
+	}
+
+	nextSlide(indice) {
+		this.setState({
+			indice: indice,
 		});
 	}
-		
+
+
+
 	render() {
 		let categoryItens;
-		if(this.state.categories){
+		if (this.state.categories) {
 			categoryItens = this.state.categories.map(category => {
 				return (
-					<CategoriaItem key={category.nome} category={category} link="/localidades/"/>
+					<CategoriaItem key={category.nome} category={category} link="/localidades/" />
 				);
 			});
 		}
 
-		return(
+		return (
 			<Container>
 				<Row>
 					<Col md="6">
-						<Carrossel>
+						<Carrossel teste={this.nextSlide}>
 						</Carrossel>
 					</Col>
 					<Col md="6">
-						<h1>LOCAL</h1>
-					Amet aliqua pariatur voluptate laboris anim officia sit in reprehenderit veniam aute cupidatat sunt ut consectetur reprehenderit laborum in ullamco eu nostrud fugiat excepteur deserunt dolor deserunt
+						<h1>{this.state.v[this.state.indice].nome}</h1>
+						{this.state.v[this.state.indice].desc}
 					</Col>
 				</Row>
-				<hr className="large-space"/>
+				<hr className="large-space" />
 				<Row>
 					{categoryItens}
 				</Row>
 			</Container>
-			);
+		);
 
-		
+
 	}
 }
 
