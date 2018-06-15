1840934a9e7b1f830dbc9a033aafd05dbe11a00b
Igor Neves Faustino <igornfaustino@gmail.com>
2018-06-15 14:20:24 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
-var user = '';
-var password = '';
+var user = 'DATABASE USER';
+var password = 'DATABASE PASSWORD';
 
 module.exports = {
 	database: 'mongodb://' + user + ':' + password + '@ds159493.mlab.com:59493/guia_cm',
 	// database: 'mongodb://localhost:27017/guia_cm',
-	secret: '48be68d59c1d7e4e2ddfdbe8d0699ae1bcf188f6',
+	secret: 'SECRET KEY',
 };
